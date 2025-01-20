4892f1c2427c69d6a67654f76a2e297f6cca8bdd
ashaInfilogix <ashainfilogix@gmail.com>
2025-01-20 16:06:03 +0530

    Adding push notification functionality

---
+++ app/Services/PushNotificationService.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\FcmToken;
+use Google\Client as GoogleClient;
+
+class PushNotificationService
+{
+    protected $projectId;
+
+    public function __construct()
+    {
+        $this->projectId = env('FIREBASE_PROJECT_ID');
+    }
+
+    public function sendNotification($userId, $title, $body)
+    {
+        $deviceTokens = FcmToken::where('user_id', $userId)->pluck('fcm_token')->toArray();
+        foreach($deviceTokens as $deviceToken) {
+            $response = $this->send($deviceToken, $title, $body);
+            print_r($response);
+        }
+        die();
+    }
+
+    protected function send($deviceToken, $title, $body)
+    {
+        $credentialsFilePath = public_path('firebase-service-account.json');
+        $client = new GoogleClient();
+        $client->setAuthConfig($credentialsFilePath);
+        $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
+        $client->refreshTokenWithAssertion();
+        $token = $client->getAccessToken();
+
+        $access_token = $token['access_token'];
+
+        $url = 'https://fcm.googleapis.com/v1/projects/'.$this->projectId.'/messages:send';
+        $data = [
+            'message' => [
+                'token' => $deviceToken,
+                'notification' => [
+                    'title' => $title,
+                    'body' => $body,
+                ],
+            ],
+        ];
+
+        $headers = [
+            'Authorization: Bearer '.$access_token,
+            'Content-Type: application/json',
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($response, true);
+    }
+}
---
+++ config/clientSite.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'sectors' => [
+        1 => 'Agriculture',
+        2 => 'Hospitality & Tourism',
+        3 => 'Airport',
+        4 => 'Individuals',
+        5 => 'Automobile',
+        6 => 'Insurance',
+        7 => 'Banking & Finance',
+        8 => 'Media & Entertainment',
+        9 => 'BPO',
+        10 => 'Mining',
+        11 => 'Education',
+        12 => 'Professional',
+        13 => 'Real Estate & Construction',
+        14 => 'Event',
+        15 => 'Retail',
+        16 => 'Gas',
+        17 => 'Trade & Distribution',
+        18 => 'Government',
+        19 => 'Transportation',
+        20 => 'Health & Pharma',
+    ],
+
+    'region_code' => [
+        1 => 'NR',
+        2 => 'SR',
+        3 =>'KGN'
+    ],
+
+    'region' => [
+        1 => 'North',
+        2 => 'South',
+        3 =>'Kingston'
+    ],
+
+    'frequency' => [
+        1 => 'Weekly',
+        2 => 'Fortnightly'
+    ]
+];
