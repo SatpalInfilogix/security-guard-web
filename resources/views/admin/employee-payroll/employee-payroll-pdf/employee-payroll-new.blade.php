8e0df6e71ce4f4a602e11c568bbd3d078616857c
AmanAharwal <amaninfilogix@gmail.com>
2025-05-09 18:36:12 +0530

    remove  monthly salary column

--- resources/views/admin/employee-payroll/employee-payroll-pdf/employee-payroll-new.blade.php
+++ resources/views/admin/employee-payroll/employee-payroll-pdf/employee-payroll-new.blade.php
@@ -71,7 +71,7 @@
     <table>
         <tr>
             <th style="width: 18%; text-align: center;">Earnings</th>
-            <th style="width: 10%; text-align: center;">Monthly Salary</th>
+            {{-- <th style="width: 10%; text-align: center;">Monthly Salary</th> --}}
             <th style="width: 8%; text-align: center;">Units</th>
             <th style="width: 10%; text-align: center;">Total Salary</th>
             <th style="width: 11%; text-align: center;">Deductions</th>
@@ -81,51 +81,56 @@
         </tr>
         <tr>
             <td>Gross Earnings</td>
-            <td style="text-align: right;">{{ formatAmount($employeePayroll->day_salary) }}</td>
-            <td style="text-align: right;">{{$employeePayroll->leave_paid > 0 ? $employeePayroll->normal_days - $employeePayroll->leave_not_paid - $employeePayroll->leave_paid: $employeePayroll->normal_days - $employeePayroll->leave_not_paid}}</td>
+            {{-- <td style="text-align: right;">{{ formatAmount($employeePayroll->day_salary) }}</td> --}}
+            <td style="text-align: right;">
+                {{ $employeePayroll->leave_paid > 0 ? $employeePayroll->normal_days - $employeePayroll->leave_not_paid - $employeePayroll->leave_paid : $employeePayroll->normal_days - $employeePayroll->leave_not_paid }}
+            </td>
             <td style="text-align: right;">{{ formatAmount($employeePayroll->normal_salary) }}</td>
             <td>{{ $employeePayroll->paye != 0 ? 'PAYE' : '' }}</td>
-            <td style="text-align: right;">{{ $employeePayroll->paye != 0 ? formatAmount($employeePayroll->paye) : '' }}</td>
+            <td style="text-align: right;">
+                {{ $employeePayroll->paye != 0 ? formatAmount($employeePayroll->paye) : '' }}</td>
             <td></td>
         </tr>
         <tr>
             <td>Leave Paid</td>
-            <td></td>
+            {{-- <td></td> --}}
             <td style="text-align: right;">{{ $employeePayroll->leave_paid }}</td>
             <td>-</td>
-            <td>{{$employeePayroll->education_tax !=0 ? 'Ed Tax': ''}}</td>
-            <td style="text-align: right;">{{$employeePayroll->education_tax !=0 ? formatAmount($employeePayroll->education_tax ): ''}}</td>
+            <td>{{ $employeePayroll->education_tax != 0 ? 'Ed Tax' : '' }}</td>
+            <td style="text-align: right;">
+                {{ $employeePayroll->education_tax != 0 ? formatAmount($employeePayroll->education_tax) : '' }}</td>
             <td></td>
             {{-- <td>{{ $employeePayroll->employer_eduction_tax }}</td> --}}
         </tr>
         <tr>
             <td>Employee Allowance</td>
-            <td></td>
+            {{-- <td></td> --}}
             <td style="text-align: right;">{{ $employeePayroll->leave_not_paid }}</td>
             <td>-</td>
-            <td>{{$employeePayroll->nis !=0 ? 'NIS': ''}}</td>
-            <td style="text-align: right;">{{$employeePayroll->nis !=0 ? formatAmount($employeePayroll->nis) : ''}}</td>
+            <td>{{ $employeePayroll->nis != 0 ? 'NIS' : '' }}</td>
+            <td style="text-align: right;">{{ $employeePayroll->nis != 0 ? formatAmount($employeePayroll->nis) : '' }}
+            </td>
             <td></td>
             {{-- <td>{{ $employeePayroll->employer_contribution_nis_tax }}</td> --}}
         </tr>
         <tr>
             @if ($employeePayroll->pending_leave_balance > 0)
                 <td>Pending Balance</td>
-                <td>{!! '&nbsp;' !!}</td>
+                {{-- <td>{!! '&nbsp;' !!}</td> --}}
                 <td style="text-align: right;">{{ $employeePayroll->pending_leave_balance }}</td>
                 <td style="text-align: right;">{{ formatAmount($employeePayroll->pending_leave_amount) }}</td>
             @else
                 <td colspan="3">{!! '&nbsp;' !!}</td>
-                <td>{!! '&nbsp;' !!}</td>
+                {{-- <td>{!! '&nbsp;' !!}</td> --}}
             @endif
-        
+
             <td>{!! $employeePayroll->nht != 0 ? 'NHT' : '&nbsp;' !!}</td>
             <td style="text-align: right;">{!! $employeePayroll->nht != 0 ? formatAmount($employeePayroll->nht) : '&nbsp;' !!}</td>
             <td>{!! '&nbsp;' !!}</td>
-        </tr>            
+        </tr>
         <tr>
             <td colspan="3"></td>
-            <td></td>
+            {{-- <td></td> --}}
             <td>{!! $employeePayroll->heart != 0 ? 'Heart' : '&nbsp;' !!}</td>
             <td></td>
             <td></td>
@@ -133,7 +138,7 @@
         </tr>
         <tr>
             <td colspan="3">{!! '&nbsp;' !!}</td>
-            <td>{!! '&nbsp;' !!}</td>
+            {{-- <td>{!! '&nbsp;' !!}</td> --}}
             <td>{!! $employeePayroll->staff_loan != 0 ? 'Staff Loan' : '&nbsp;' !!}</td>
             <td style="text-align: right;">{!! $employeePayroll->staff_loan != 0 ? formatAmount($employeePayroll->staff_loan) : '&nbsp;' !!}</td>
             <td style="text-align: right;">{!! $employeePayroll->pending_staff_loan != 0 ? number_format($employeePayroll->pending_staff_loan) : '&nbsp;' !!}</td>
@@ -141,42 +146,44 @@
         </tr>
         <tr>
             <td colspan="3">{!! '&nbsp;' !!}</td>
-            <td>{!! '&nbsp;' !!}</td>
+            {{-- <td>{!! '&nbsp;' !!}</td> --}}
             <td>{!! $employeePayroll->medical_insurance != 0 ? 'Medical Ins' : '&nbsp;' !!}</td>
-            <td style="text-align: right;">{!! $employeePayroll->medical_insurance != 0 ? formatAmount($employeePayroll->medical_insurance):'&nbsp;' !!}</td>
-            <td style="text-align: right;">{!! $employeePayroll->medical_insurance != 0 ?number_format($employeePayroll->pending_medical_insurance):'&nbsp;' !!}</td>
+            <td style="text-align: right;">{!! $employeePayroll->medical_insurance != 0 ? formatAmount($employeePayroll->medical_insurance) : '&nbsp;' !!}</td>
+            <td style="text-align: right;">{!! $employeePayroll->medical_insurance != 0
+                ? number_format($employeePayroll->pending_medical_insurance)
+                : '&nbsp;' !!}</td>
             {{-- <td></td> --}}
         </tr>
+        @php
+            $total = $employeePayroll->gross_salary ?? 0;
+            $totalAmount =
+                $employeePayroll->paye +
+                $employeePayroll->education_tax +
+                $employeePayroll->nis +
+                $employeePayroll->nht +
+                $employeePayroll->staff_loan +
+                $employeePayroll->medical_insurance +
+                $employeePayroll->salary_advance +
+                $employeePayroll->approved_pension_scheme +
+                $employeePayroll->psra +
+                $employeePayroll->bank_loan +
+                $employeePayroll->missing_goods +
+                $employeePayroll->damaged_goods +
+                $employeePayroll->garnishment;
+        @endphp
         <tr>
             <th>Total (JMD)</th>
+            {{-- <td></td> --}}
             <td></td>
-            <td></td>
-            @php
-                $total = $employeePayroll->gross_salary ?? 0;
-                $totalAmount =
-                    $employeePayroll->paye +
-                    $employeePayroll->education_tax +
-                    $employeePayroll->nis +
-                    $employeePayroll->nht +
-                    $employeePayroll->staff_loan +
-                    $employeePayroll->medical_insurance +
-                    $employeePayroll->salary_advance +
-                    $employeePayroll->approved_pension_scheme +
-                    $employeePayroll->psra +
-                    $employeePayroll->bank_loan +
-                    $employeePayroll->missing_goods +
-                    $employeePayroll->damaged_goods +
-                    $employeePayroll->garnishment;
-            @endphp
             <td style="text-align: right;">{{ formatAmount($total) }}</td>
             <td></td>
-            <td style="text-align: right;">{!! $employeePayroll->totalAmount != 0 ? formatAmount($totalAmount) :'&nbsp;' !!}</td>
+            <td style="text-align: right;">{!! $employeePayroll->totalAmount != 0 ? formatAmount($totalAmount) : '&nbsp;' !!}</td>
             <td></td>
             {{-- <td></td> --}}
         </tr>
         <tr>
             <td></td>
-            <td></td>
+            {{-- <td></td> --}}
             <td></td>
             <td></td>
             <th>Net Salary (JMD)</th>
